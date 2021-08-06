533e55d2b75279f469837355841e9e2d335662d7
tkabeee <tkabkondo@gmail.com>
2021-08-07 00:25:57 +0900

    Update convertToPost.ts

--- src/lib/notion/convertToPost.ts
+++ src/lib/notion/convertToPost.ts
@@ -5,32 +5,32 @@ import { getPostPath, getPostUrl } from '../helpers/blog-helper'
 export async function convertToPost(page: INotionPage): Promise<IPost> {
   const { id, created_time, last_edited_time, properties: props } = page
 
-  const Page: string = props.Page.title[0].plain_text
-  const Slug: string = props.Slug.select.name
-  const Description: string = props.Description.rich_text[0]?.plain_text || ''
-  const Category: string = props.Category.select.name
-  const Tag: string[] = props.Tag.multi_select.map(
+  const title: string = props.Page.title[0].plain_text
+  const slug: string = props.Slug.select.name
+  const description: string = props.Description.rich_text[0]?.plain_text || ''
+  const category: string = props.Category?.select.name
+  const tags: string[] = props.Tags?.multi_select.map(
     (item: { name: string }) => item.name
   )
-  const Date: number = props.Date.date.start
-  const CreatedAt: string = created_time
-  const UpdatedAt: string = last_edited_time
-  const Published: boolean = props.Published.checkbox
-  const Url: string = getPostUrl(Slug)
-  const Path: string = getPostPath(Slug)
+  const date: number = props.Date.date.start
+  const createdAt: string = created_time
+  const updatedAt: string = last_edited_time
+  const published: boolean = props.Published.checkbox
+  const url: string = getPostUrl(slug)
+  const path: string = getPostPath(slug)
 
   return {
     id,
-    Page,
-    Slug,
-    Description,
-    Category,
-    Tag,
-    Date,
-    CreatedAt,
-    UpdatedAt,
-    Published,
-    Url,
-    Path,
+    title,
+    slug,
+    description,
+    category,
+    tags,
+    date,
+    createdAt,
+    updatedAt,
+    published,
+    url,
+    path,
   }
 }
