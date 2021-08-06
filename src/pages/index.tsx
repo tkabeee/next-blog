46974b312b3dcd50b510a91ff0978ac50da2b2f6
tkabeee <tkabkondo@gmail.com>
2021-08-06 14:55:28 +0900

    アロー関数に統一したい

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const Index = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -50,3 +50,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Index
