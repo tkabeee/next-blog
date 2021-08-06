e14a73836b45a1139a8fd8407126bb0dff1aaeeb
tkabeee <tkabkondo@gmail.com>
2021-08-06 15:21:31 +0900

    set env notion integration token

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
+    NOTION_INTEGRATION_TOKEN: process.env.NOTION_INTEGRATION_TOKEN,
     BASE_URL: process.env.BASE_URL,
     GTM_ID: process.env.GTM_ID,
     USER_LOCALE: process.env.USER_LOCALE,
