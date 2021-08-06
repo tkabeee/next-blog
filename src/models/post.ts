2390b825fb0d4f9dd243cd0481cc22c29e534606
tkabeee <tkabkondo@gmail.com>
2021-08-06 23:47:38 +0900

    投稿データの型を定義

---
+++ src/models/post.ts
@@ -0,0 +1,14 @@
+export interface IPost {
+  id: string
+  Page: string
+  Slug: string
+  Description: string
+  Category: string
+  Tag: string[]
+  Date: number
+  CreatedAt: string
+  UpdatedAt: string
+  Published: boolean
+  Url: string
+  Path: string
+}
