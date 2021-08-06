5fcd8538790768fe3f8fe2a47b736d997e87b0d4
tkabeee <tkabkondo@gmail.com>
2021-08-06 13:46:55 +0900

    クライアントサイドで使用する環境変数をセット

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    BASE_URL: process.env.BASE_URL,
+    GTM_ID: process.env.GTM_ID,
+    USER_LOCALE: process.env.USER_LOCALE,
+    USER_TIMEZONE: process.env.USER_TIMEZONE,
+  },
   distDir: 'build',
   cleanDistDir: true,
   images: {
