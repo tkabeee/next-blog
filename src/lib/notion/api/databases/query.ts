75a0ffb1a95598b8ec9f4a27b11d57a305ab40b6
tkabeee <tkabkondo@gmail.com>
2021-08-06 15:48:42 +0900

    create module notion api database query

---
+++ src/lib/notion/api/databases/query.ts
@@ -0,0 +1,24 @@
+// Reference
+// https://developers.notion.com/reference/database
+
+import { notion } from '../../client'
+import { INotionPage } from '../../../../models/notion/page'
+
+export interface IResponse {
+  object: string
+  results: INotionPage[]
+  has_more: boolean
+  next_cursor: string
+}
+
+export async function query(params: any): Promise<IResponse> {
+  const { filter, sorts, startCursor, pageSize } = params
+
+  return await notion.databases.query({
+    database_id: process.env.BLOG_INDEX_ID,
+    filter: filter,
+    sorts: sorts,
+    start_cursor: startCursor || undefined,
+    page_size: pageSize || 100,
+  })
+}
