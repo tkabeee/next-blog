f2f832e96c37f667229d9c8f39164f5ec9b965d6
tkabeee <tkabkondo@gmail.com>
2021-08-10 22:42:00 +0900

    create constant for notion

---
+++ src/lib/notion/constants.js
@@ -0,0 +1,27 @@
+// https://github.com/ijjk/notion-blog/blob/main/src/lib/notion/server-constants.js
+
+// use commonjs so it can be required without transpiling
+const path = require('path')
+
+const normalizeId = (id) => {
+  if (!id) return id
+  if (id.length === 36) return id
+  if (id.length !== 32) {
+    throw new Error(
+      `Invalid blog-index-id: ${id} should be 32 characters long. Info here https://github.com/ijjk/notion-blog#getting-blog-index-and-token`
+    )
+  }
+  return `${id.substr(0, 8)}-${id.substr(8, 4)}-${id.substr(12, 4)}-${id.substr(
+    16,
+    4
+  )}-${id.substr(20)}`
+}
+
+const NOTION_TOKEN = process.env.NOTION_TOKEN
+const API_ENDPOINT = 'https://www.notion.so/api/v3'
+
+module.exports = {
+  NOTION_TOKEN,
+  API_ENDPOINT,
+  normalizeId,
+}
