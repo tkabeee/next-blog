8eef08312c5806571ee899e036aa2c1fb068c30e
tkabeee <tkabkondo@gmail.com>
2021-08-10 22:19:18 +0900

    create notion-page component

---
+++ src/components/notion-page.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Page = styled.div`
+  display: flex;
+`
+
+export const NotionPage = () => {
+  return (
+    <Page>
+      page
+    </Page>
+  )
+}
