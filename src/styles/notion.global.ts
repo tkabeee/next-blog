4ce94afb8929fb0a462c9be43f82215e8600bb04
tkabeee <tkabkondo@gmail.com>
2021-08-13 16:54:29 +0900

    change to typescript

---
+++ src/styles/notion.global.ts
@@ -0,0 +1,26 @@
+export const fontFamily: string =
+  "-apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, 'Apple Color Emoji', Arial, sans-serif, 'Segoe UI Emoji', 'Segoe UI Symbol'"
+
+export const fontColors: any = {
+  gray: 'rgba(55, 53, 47, 0.6)',
+  brown: 'rgb(100, 71, 58)',
+  orange: 'rgb(217, 115, 13)',
+  yellow: 'rgb(223, 171, 1)',
+  teal: 'rgb(15, 123, 108)',
+  blue: 'rgb(11, 110, 153)',
+  purple: 'rgb(105, 64, 165)',
+  pink: 'rgb(173, 26, 114)',
+  red: 'rgb(224, 62, 62)',
+}
+
+export const backgroundColors: any = {
+  gray_background: 'rgb(235, 236, 237)',
+  brown_background: 'rgb(233, 229, 227)',
+  orange_background: 'rgb(250, 235, 221)',
+  yellow_background: 'rgb(251, 243, 219)',
+  teal_background: 'rgb(221, 237, 234)',
+  blue_background: 'rgb(221, 235, 241)',
+  purple_background: 'rgb(234, 228, 242)',
+  pink_background: 'rgb(244, 223, 235)',
+  red_background: 'rgb(251, 228, 228)',
+}
