da57e99b3b5c85a7fde6e21cc1580ccb75fa56b8
tkabeee <tkabkondo@gmail.com>
2021-08-14 10:49:59 +0900

    Update dynamic.tsx

--- src/components/dynamic.tsx
+++ src/components/dynamic.tsx
@@ -4,17 +4,17 @@ import dynamic from 'next/dynamic'
 import { ExtLink } from './ext-link'
 import { Highlighter } from './highlighter'
 
-interface Props {
-    ol: string
-    ul: string
-    li: string
-    p: string
-    blockquote: string
-    a: (props: any) => JSX.Element
-    h: (props: any) => JSX.Element
-    Code: React.ReactNode
-    Counter: React.ReactNode
-    Equation: React.ReactNode
+export interface Props {
+  ol: string
+  ul: string
+  li: string
+  p: string
+  blockquote: string
+  a: (props: any) => JSX.Element
+  h: (props: any) => JSX.Element
+  Code: React.ReactNode
+  Counter: React.ReactNode
+  Equation: React.ReactNode
 }
 
 export const components: Props = {
@@ -26,7 +26,6 @@ export const components: Props = {
   blockquote: 'blockquote',
   a: ExtLink,
   h: Highlighter,
-
   Code: dynamic(() => import('./code')),
   Counter: dynamic(() => import('./counter')),
   Equation: dynamic(() => import('./equation')),
