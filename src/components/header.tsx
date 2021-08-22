b8ae7f05d15c2a49802ed9e8c4e486f24770a96e
tkabeee <tkabkondo@gmail.com>
2021-08-22 16:15:47 +0900

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,34 @@
+import Link from 'next/link'
+import styled from 'styled-components'
+
+export const NavBar = styled.nav`
+  position: sticky;
+  top: 0;
+  z-index: var(--zindex-sticky);
+  padding: var(--navbar-padding-vertical) var(--navbar-padding-horizontal);
+  width: 100%;
+  height: var(--navbar-height);
+  background-color: var(--navbar-background-color);
+`
+
+export const NavBarInner = styled.div``
+export const NavBarItems = styled.div``
+export const NavBarTitle = styled.strong``
+
 export const Header = () => {
   return (
     <header>
+      <NavBar>
+        <NavBarInner>
+          <NavBarItems>
+            <Link href={'/'} as={'/'} key={1}>
+              <a className='flex items-center mr-12'>
+                <NavBarTitle>Next Blog</NavBarTitle>
+              </a>
+            </Link>
+          </NavBarItems>
+        </NavBarInner>
+      </NavBar>
     </header>
   )
 }
