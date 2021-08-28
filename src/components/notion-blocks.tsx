b821ba93e452af424073990a8d446862aad1b3c1
tkabeee <tkabkondo@gmail.com>
2021-08-28 10:08:33 +0900

    Update notion-blocks.tsx

--- src/components/notion-blocks.tsx
+++ src/components/notion-blocks.tsx
@@ -1,7 +1,11 @@
-import { NotionParagraph } from "./notion-blocks/paragraph";
-import { NotionHeader } from "./notion-blocks/header";
+import { NotionParagraph } from './notion-blocks/paragraph'
+import { NotionHeader } from './notion-blocks/header'
+import { NotionSubHeader } from './notion-blocks/sub-header'
+import { NotionSubSubHeader } from './notion-blocks/sub-sub-header'
 
 export {
   NotionParagraph,
   NotionHeader,
+  NotionSubHeader,
+  NotionSubSubHeader,
 }
