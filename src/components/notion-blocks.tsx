b7a5c207b1bb19a2a6c6b108fdabd88f9714b0dd
tkabeee <tkabkondo@gmail.com>
2021-08-28 10:50:38 +0900

    add component to summary block component

--- src/components/notion-blocks.tsx
+++ src/components/notion-blocks.tsx
@@ -2,10 +2,12 @@ import { NotionParagraph } from './notion-blocks/paragraph'
 import { NotionHeader } from './notion-blocks/header'
 import { NotionSubHeader } from './notion-blocks/sub-header'
 import { NotionSubSubHeader } from './notion-blocks/sub-sub-header'
+import { NotionBulletedList } from './notion-blocks/bulleted-list'
 
 export {
   NotionParagraph,
   NotionHeader,
   NotionSubHeader,
   NotionSubSubHeader,
+  NotionBulletedList,
 }
