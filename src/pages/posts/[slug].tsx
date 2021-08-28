cfe93d7e0ae6977e3670d238e70a4efb7a47edba
tkabeee <tkabkondo@gmail.com>
2021-08-28 16:06:45 +0900

    format using prettier

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -4,11 +4,11 @@ import { Header } from '../../components/header'
 import { NotionPage } from '../../components/notion-page'
 
 import { IPost } from '../../models/post'
-import { INotionPage } from "../../models/notion/page"
+import { INotionPage } from '../../models/notion/page'
 import { INotionPageChunk } from '../../models/notion/page-chunk'
 import { REVALIDATE_SECONDS_BLOG_POST } from '../../lib/constants'
-import { getAllBlogSlugs } from "../../lib/notion/getAllBlogSlugs"
-import { getPageBySlug } from "../../lib/notion/getPage"
+import { getAllBlogSlugs } from '../../lib/notion/getAllBlogSlugs'
+import { getPageBySlug } from '../../lib/notion/getPage'
 import { convertToPost } from '../../lib/notion/convertToPost'
 
 import { formatDateStr } from '../../lib/helpers/blog-helper'
@@ -22,7 +22,7 @@ type Props = {
   post: IPost
 }
 
-export const getStaticPaths: GetStaticPaths<Params> = async() => {
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
   const list: string[] = await getAllBlogSlugs()
   const paths = list.map((slug) => {
     return { params: { slug: slug } }
@@ -34,7 +34,9 @@ export const getStaticPaths: GetStaticPaths<Params> = async() => {
   }
 }
 
-export const getStaticProps: GetStaticProps<Props, Params> = async({ params }) => {
+export const getStaticProps: GetStaticProps<Props, Params> = async ({
+  params,
+}) => {
   const page: INotionPage = await getPageBySlug(params!.slug)
 
   const post: IPost = await convertToPost(page)
@@ -49,22 +51,28 @@ export const getStaticProps: GetStaticProps<Props, Params> = async({ params }) =
   }
 }
 
-const RenderPost = ({ post, pageData }: { post: IPost, pageData: INotionPageChunk}) => {
+const RenderPost = ({
+  post,
+  pageData,
+}: {
+  post: IPost
+  pageData: INotionPageChunk
+}) => {
   return (
     <>
       <Header />
       <article>
-        <section className='break-words'>
-          <div className='flex justify-center'>
-            <div className='w-full max-w-2xl'>
-              <h1 className='mt-8 text-5xl'>{post?.title}</h1>
+        <section className="break-words">
+          <div className="flex justify-center">
+            <div className="w-full max-w-2xl">
+              <h1 className="mt-8 text-5xl">{post?.title}</h1>
               {post?.date && (
                 <div className="posted">Posted: {formatDateStr(post.date)}</div>
               )}
             </div>
           </div>
-          <div className='flex justify-center'>
-            <div className='w-full max-w-2xl'>
+          <div className="flex justify-center">
+            <div className="w-full max-w-2xl">
               {pageData && <NotionPage data={pageData} />}
             </div>
           </div>
