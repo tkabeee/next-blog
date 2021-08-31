3d18d66f559e4bb73839e891836a3eb6c9b08356
tkabeee <tkabkondo@gmail.com>
2021-08-31 21:03:29 +0900

    videoデータがyoutubeのembed用URLの場合の対応

--- src/components/notion-blocks/video.tsx
+++ src/components/notion-blocks/video.tsx
@@ -8,8 +8,6 @@ interface Props {
   source: string
 }
 
-const videoBgColor = 'rgb(242, 241, 238)'
-
 const Block = styled.div`
   align-self: center;
   margin-top: 4px;
@@ -17,11 +15,42 @@ const Block = styled.div`
   max-width: 100%;
 `
 
-const Video = styled.video`
-  display: block;
+const FrameHeightAdjust = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
   width: 100%;
-  background-color: ${videoBgColor};
+  min-height: 100px;
+  height: 0px;
+  padding-bottom: 56.2061%;
 `
+
+const FramePosition = styled.div`
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 100%;
+  height: 100%;
+  border-radius: 1px;
+`
+
+const Frame = styled.iframe`
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 100%;
+  height: 100%;
+  border-radius: 1px;
+  pointer-events: auto;
+  background-color: rgb(247, 246, 245);
+`
+
+const contentStyle = {
+  display: 'block',
+  width: '100%',
+  PointerEvents: 'none',
+  backgroundColor: 'rgb(242, 241, 238)',
+}
 
 export const NotionVideo = ({ id, width, source }: Props) => {
   return (
@@ -31,12 +60,31 @@ export const NotionVideo = ({ id, width, source }: Props) => {
           className="notion-cursor-default"
           style={{ position: 'relative', overflow: 'hidden', flexGrow: 1 }}
         >
-          <Video
-            src={`${getApiAssetPath(source, id)}#t=0.001`}
-            playsInline
-            controls
-            preload="metadata"
-          />
+          {['mp4'].some((ext) => source.endsWith(`.${ext}`)) ? (
+            <video
+              style={contentStyle}
+              src={`${getApiAssetPath(source, id)}#t=0.001`}
+              playsInline
+              controls
+              preload="metadata"
+            />
+          ) : (
+            <div style={contentStyle}>
+              <FrameHeightAdjust>
+                <FramePosition>
+                  <div style={{ width: '100%', height: '100%' }}>
+                    <Frame
+                      src={source}
+                      sandbox="allow-scripts allow-popups allow-top-navigation-by-user-activation allow-forms allow-same-origin"
+                      allowFullScreen
+                      loading="lazy"
+                      frameBorder="0"
+                    />
+                  </div>
+                </FramePosition>
+              </FrameHeightAdjust>
+            </div>
+          )}
         </div>
       </div>
     </Block>
