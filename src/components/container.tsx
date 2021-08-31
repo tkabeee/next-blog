73d0eb71d964768cfe0ed88182363b8287e254d9
tkabeee <tkabkondo@gmail.com>
2021-08-31 21:29:42 +0900

    モバイルでコンテンツ幅がオーバーフローしていたので改善

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -5,8 +5,8 @@ import { spacingUnit, pageMaxWidth } from '../styles/notion.global'
 export const Container = styled.section`
   padding-left: ${Math.floor(spacingUnit * 6)}px;
   padding-right: ${Math.floor(spacingUnit * 6)}px;
-  width: ${pageMaxWidth}px;
-  max-width: 100%;
+  width: 100%;
+  max-width: ${pageMaxWidth}px;
 
   @media (max-width: 640px) {
     padding-left: ${Math.floor(spacingUnit * 1.5)}px;
