e765e4c67853fab13a9bee62bc5b7633828ab1b3
Takayuki Kondo <tkabeee@users.noreply.github.com>
2023-08-06 15:19:37 +0900

    move to styled component

--- src/components/styled/layout.tsx
+++ src/components/styled/layout.tsx
@@ -2,6 +2,8 @@
 
 import styled from 'styled-components'
 
+import { spacingUnit, pageMaxWidth } from '@/styles/notion.global'
+
 export const Main = styled.main`
   display: flex;
   flex-direction: column;
@@ -17,4 +19,16 @@ export const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   min-height: 100vh;
+`
+
+export const Container = styled.section`
+  padding-left: ${Math.floor(spacingUnit * 6)}px;
+  padding-right: ${Math.floor(spacingUnit * 6)}px;
+  width: 100%;
+  max-width: ${pageMaxWidth}px;
+
+  @media (max-width: 640px) {
+    padding-left: ${Math.floor(spacingUnit * 1.5)}px;
+    padding-right: ${Math.floor(spacingUnit * 1.5)}px;
+  }
 `
