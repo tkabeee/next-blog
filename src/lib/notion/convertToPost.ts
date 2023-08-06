782bd27e78311323f6f56103a640a1651fdd27fa
Takayuki Kondo <tkabeee@users.noreply.github.com>
2023-08-06 22:52:52 +0900

    Update convertToPost.ts

--- src/lib/notion/convertToPost.ts
+++ src/lib/notion/convertToPost.ts
@@ -8,7 +8,7 @@ export async function convertToPost(page: INotionPage): Promise<IPost> {
   const title: string = props.Page.title[0].plain_text
   const slug: string = props.Slug.select.name
   const description: string = props.Description.rich_text[0]?.plain_text || null
-  const category: string = props.Category?.select.name || null
+  const category: string = props.Category?.select?.name || null
   const tags: string[] =
     props.Tags?.multi_select.map((item: { name: string }) => item.name) || []
   const date: number = props.Date.date.start
